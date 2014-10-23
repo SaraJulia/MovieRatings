476f5455315b0b5a31d063650d9dc91242a5102f
Hackbright Student <info@hackbrightacademy.com>
2014-10-23 12:32:56 -0700

    Files in db.

--- model.py
+++ model.py
@@ -1,11 +1,35 @@
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, DateTime
 
 Base = declarative_base()
 
 ### Class declarations go here
 
+class User(Base):
+    __tablename__ = 'users'
+
+    user_id =   Column(Integer, primary_key = True)
+    email =     Column(String(64), nullable = True)
+    password =  Column(String(64), nullable = True)
+    age =       Column(Integer, nullable = True)
+    zipcode =   Column(String(15), nullable = True)
+
+class Movie(Base):
+    __tablename__ = 'movies'
+
+    movie_id =      Column(Integer, primary_key = True)
+    title =         Column(String(64), nullable = True)
+    release_date =  Column(DateTime, nullable = True)
+    imdb =          Column(String(15), nullable = True)
+
+class Rating(Base):
+    __tablename__ = 'ratings'
+
+    rating_id =     Column(Integer, primary_key = True)
+    movie_id =      Column(Integer, nullable = True)
+    user_id =       Column(Integer, nullable = True)
+    rating =        Column(Integer,nullable = True)
 
 ### End class declarations
 
