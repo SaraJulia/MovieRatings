da635109c1ed7b3a4443a2587e613dae83b19025
Hackbright Student <info@hackbrightacademy.com>
2014-10-23 17:41:17 -0700

    All csv files loaded into database.

--- seed.py
+++ seed.py
@@ -1,21 +1,69 @@
 import model
 import csv
+from datetime import datetime
 
-def load_users(session):
+def load_users(session, filename): 
     # use u.user
-    pass
+    with open(filename, 'rb') as csvfile:
+        
+        lines = csv.reader(csvfile, delimiter = '|')
+        
+        for line in lines:
+            user = model.User() 
+            user.user_id = line[0].strip()
+            user.age = line[1].strip()
+            user.zipcode = line[4].strip()
 
-def load_movies(session):
+            session.add(user)
+
+
+    session.commit()
+
+
+
+def load_movies(session, filename):
     # use u.item
-    pass
+    # would like to improve date format but not a big deal
+    with open(filename, 'rb') as csvfile:
+        
+        lines = csv.reader(csvfile, delimiter = '|')
+        
+        for line in lines:
+            movie = model.Movie()
+            movie.movie_id = line[0].strip()
+            movie.title = line[1].strip()
+            movie.title = movie.title[:-6] #removing year from title
+            movie.title = movie.title.decode("latin-1")
+            movie.release_date = datetime.strptime(line[2].strip(),"%d-%b-%Y")
+            movie.imdb = line[4].strip()
+            session.add(movie)
 
-def load_ratings(session):
+
+    session.commit()
+    
+
+
+def load_ratings(session, filename): 
     # use u.data
-    pass
+    # when running for real, remove echo = true
+    with open(filename, 'rb') as temp_file:
+
+        for line in temp_file:
+            line = line.split()
+            rating = model.Rating() 
+            rating.movie_id = line[0].strip()
+            rating.user_id = line[1].strip()
+            rating.rating = line[2].strip()
+            session.add(rating)
+
+    session.commit()
 
 def main(session):
+    # when running for real, remove echo = true
     # You'll call each of the load_* functions with the session as an argument
-    pass
+    # load_movies(session, 'seed_data/u.item')
+    # load_ratings(session, 'seed_data/u.data')
+    # load_users(session,'seed_data/u.user')
 
 if __name__ == "__main__":
     s= model.connect()
