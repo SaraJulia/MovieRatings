f1e7ab3fe5c42f040e788d580dd01dcdaa5c2271
Hackbright Student <info@hackbrightacademy.com>
2014-10-29 13:25:38 -0700

    Created the correlation and prediction methods. Discovered that we loaded the ratings table incorrectly.

--- model.py
+++ model.py
@@ -2,6 +2,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
 from sqlalchemy import Column, Integer, String, Date, ForeignKey
 from sqlalchemy.orm import sessionmaker, relationship, backref, scoped_session
+import correlation
 
 # ENGINE = None
 # Session = None
@@ -25,6 +26,53 @@ class User(Base):
     password =  Column(String(64), nullable = True)
     age =       Column(Integer, nullable = True)
     zipcode =   Column(String(15), nullable = True)
+
+    def similarity(self,user2):
+        u_ratings = {}
+        paired_ratings = []
+        for r in self.ratings:
+            u_ratings[r.movie_id] = r
+
+
+        #print "user2: ", user2
+        for r in user2.ratings:
+            u_r = u_ratings.get(r.movie_id)
+            if u_r:
+                paired_ratings.append( (u_r.rating, r.rating) )
+
+        if paired_ratings:
+            return correlation.pearson(paired_ratings)
+        else:
+            return 0.0
+
+    def predict_rating(self, movie):
+        #m = session.query(Movie).filter_by(title=title).one()
+
+        # ratings = self.ratings
+
+        other_ratings = movie.ratings
+        other_users = []
+
+        for r in other_ratings:
+            other_users.append(r.user)
+
+     #   print "other users: ", other_users
+
+        user_correlations = []
+        for other_user in other_users:
+            print other_user
+            similarity = self.similarity(other_user)
+            pair = (similarity, other_user.user_id)
+            user_correlations.append(pair)
+
+        user_correlations.sort(reverse = True)
+
+        best_match = user_correlations[0]
+        print best_match
+        # best_match_user_id = session.query(Rating).filter_by(user_id = best_match[1]).one()
+        # prediction = best_match[0] * best_match_user_id.rating
+
+        # return prediction
 
 class Movie(Base):
     __tablename__ = 'movies'
